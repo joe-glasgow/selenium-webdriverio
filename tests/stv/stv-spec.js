cfa7caeb79f0c5fedf03d77a013d836c52c3dd01
Joseph Morrison <jmorri35@gmail.com>
2016-05-17 15:52:19 +0100

    changing tests to work with v3 async

--- tests/stv/stv-spec.js
+++ tests/stv/stv-spec.js
@@ -4,21 +4,15 @@ function myTest(site) {
     describe('Account :: ', function () {
 
                 describe('Sign in - As a user', function () {
-                    before(function (done) {
-                        // local env is SLOOWWWWWWWWW
-                        this.timeout(40000);
-                        browser.url('http://' + site, done);
-                    });
 
-                    it('I can click sign in and see a sign modal', function (done) {
-                        this.timeout(40000);
-                        browser
-                            // for dynamic bars
-                            .pause(3000)
-                            .click('body')
-
-                            .then(function(value) {
-                                assert.equal(true, true);
+                    it('I can click sign in and see a sign modal', function async (done) {
+                        //browser.url('http://' + site, done)
+                            return browser
+                            .timeouts('page load', 40000)
+                            .url('http://' + site)
+                            .getTitle().then(function(title) {
+                                console.log(title);
+                                assert.equal(true, true)
                             })
                             .call(done);
                     });
