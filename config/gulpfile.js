74c165f5eae22d7961a6419a3faeb78804f899c7
Joseph Morrison <jmorri35@gmail.com>
2016-05-19 16:54:10 +0100

    changes for windows

--- config/gulpfile.js
+++
@@ -1,5 +0,0 @@
-var gulp = require('gulp');
-// install selenium
-gulp.task('install-selenium', require('./tools/install-selenium'));
-
-gulp.task('browser-test', ['install-selenium'], require('./tools/browser-test'));
